228a6f74680f25c14c587713ee1db44fc48f02fc
Milo <pymc20@gmail.com>
2026-02-07 12:43:08 +0900

    Add Zigzag Conversion solution with tests and CMake updates

---
+++ cpp/0001-0100/0006/0006.cpp
@@ -0,0 +1,67 @@
+//
+// Created by Milo on 2/5/26.
+//
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  string convert(string s, int numRows) {
+    vector<vector<char>> resultVector(numRows, vector<char>(s.length(), '-'));
+    string result = "";
+    int columnIndex = 0;
+    for (int i = 0; i < s.length(); i++) {
+      resultVector[columnIndex].push_back(s[i]);
+      if (numRows != 1) {
+        if ((i / (numRows - 1)) % 2 != 0) {
+          columnIndex--;
+        } else {
+          columnIndex++;
+        }
+      }
+    }
+    for (int i = 0; i < numRows; i++) {
+      for (char c : resultVector[i]) {
+        if (c != '-') result += c;
+      }
+    }
+    return result;
+  }
+};
+
+int main() {
+  Solution sol;
+
+  // Test Case 1
+  cout << "Test Case 1: " << endl;
+  string result = sol.convert("PAYPALISHIRING", 3);
+  string result_expected = "PAHNAPLSIIGYIR";
+  cout << "result : " << result << endl;
+  cout << "result_expected : " << result_expected << endl;
+  assert(result == result_expected);
+  cout << "PASSED" << endl;
+
+  // Test Case 2
+  cout << "Test Case 2: " << endl;
+  result = sol.convert("PAYPALISHIRING", 4);
+  result_expected = "PINALSIGYAHRPI";
+  cout << "result : " << result << endl;
+  cout << "result_expected : " << result_expected << endl;
+  assert(result == result_expected);
+  cout << "PASSED" << endl;
+
+  // Test Case 3
+  cout << "Test Case 3: " << endl;
+  result = sol.convert("A", 1);
+  result_expected = "A";
+  cout << "result : " << result << endl;
+  cout << "result_expected : " << result_expected << endl;
+  assert(result == result_expected);
+  cout << "PASSED" << endl;
+
+  return 0;
+}
---
+++ cpp/0001-0100/0006/README.md
@@ -0,0 +1,13 @@
+# 6. Zigzag Conversion
+
+[Problem Link](https://leetcode.com/problems/zigzag-conversion/description/)
+
+### 💡 Approach & Retrospective
+
+- **Initial Approach (Intuitive but Inefficient):**:
+  - I initially attempted a solution using a highly intuitive approach.
+  - While the efficiency was sub-optimal, it successfully passed all test cases.
+  - The implementation felt straightforward, though I prioritized functionality over performance during this phase.
+- **Future Improvement (Refactoring)**:
+  - I plan to revisit this code later to optimize the performance.
+  - Goal: I will refactor the current logic to improve efficiency and refine the algorithm into a more sophisticated structure.
