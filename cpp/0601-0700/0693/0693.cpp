31332a3e022b8f54837debb019d7df8d587820be
Milo <pymc20@gmail.com>
2026-02-18 11:50:18 +0900

    Add Binary Number with Alternating Bits solution with tests and CMake updates

---
+++ cpp/0601-0700/0693/0693.cpp
@@ -0,0 +1,54 @@
+//
+// Created by Milo on 2/18/26.
+//
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+  bool hasAlternatingBits(int n) {
+    while (n > 0) {
+      int last_bit = n & 1;
+      n >>= 1;
+      if (last_bit == (n & 1)) return false;
+    }
+    return true;
+  }
+};
+
+int main() {
+  Solution sol;
+
+  // Test Case 1
+  cout << "Test Case 1: " << endl;
+  bool result = sol.hasAlternatingBits(5);
+  bool result_expected = true;
+  cout << "result : " << result << endl;
+  cout << "result_expected : " << result_expected << endl;
+  assert(result == result_expected);
+  cout << "PASSED" << endl;
+
+  // Test Case 2
+  cout << "Test Case 2: " << endl;
+  result = sol.hasAlternatingBits(7);
+  result_expected = false;
+  cout << "result : " << result << endl;
+  cout << "result_expected : " << result_expected << endl;
+  assert(result == result_expected);
+  cout << "PASSED" << endl;
+
+
+  // Test Case 2
+  cout << "Test Case 2: " << endl;
+  result = sol.hasAlternatingBits(11);
+  result_expected = false;
+  cout << "result : " << result << endl;
+  cout << "result_expected : " << result_expected << endl;
+  assert(result == result_expected);
+  cout << "PASSED" << endl;
+
+  return 0;
+}
---
+++ cpp/0601-0700/0693/README.md
@@ -0,0 +1,8 @@
+# 693. Binary Number with Alternating Bits
+
+[Problem Link](https://leetcode.com/problems/binary-number-with-alternating-bits/description/)
+
+### Review
+
+This problem is very simple if you understand bitwise operations.
+I misunderstood the operator precedence, so I fixed it and passed the problem.
