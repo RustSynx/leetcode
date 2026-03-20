11d2503cfd6670ae90672873426d003b63a218dc
Milo <pymc20@gmail.com>
2026-03-21 00:20:38 +0900

    Update Minimum Absolute Difference in Sliding Submatrix solution with refactored implementation, updated tests, and CMake changes

--- cpp/3501-3600/3567/3567.cpp
+++ cpp/3501-3600/3567/3567.cpp
@@ -1,65 +1,46 @@
 //
-// Created by Milo on 3/12/26.
+// Created by Milo on 3/21/26.
 //
 #include <algorithm>
 #include <cassert>
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
-struct UnionFind {
-  vector<int> parent;
-  UnionFind(int node_count) {
-    parent.resize(node_count);
-    for (int i = 0; i < node_count; i++) {
-      parent[i] = i;
-    }
-  }
-  int find(int x) {
-    if (parent[x] != x) {
-      parent[x] = find(parent[x]);
-    }
-    return parent[x];
-  }
-};
-
 class Solution {
- public:
-  int maxStability(int n, vector<vector<int>>& edges, int k) {
-    UnionFind union_find(n);
-    sort(edges.begin(), edges.end(),
-         [](const vector<int>& a, const vector<int>& b) {
-           if (a[3] != b[3]) return a[3] > b[3];
-           return a[2] > b[2];
-         });
-    vector<int> essential_weight;
-    essential_weight.reserve(edges.size());
-    vector<int> upgrade_weight;
-    upgrade_weight.reserve(edges.size());
-    for (int i = 0; i < edges.size(); i++) {
-      int root_one = union_find.find(edges[i][0]);
-      int root_two = union_find.find(edges[i][1]);
-      if (edges[i][3] == 1 && root_one == root_two) {
-        return -1;
+public:
+  vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int k) {
+    int m = grid.size();
+    int n = grid[0].size();
+
+    vector<vector<int>> ans(m - k + 1, vector<int>(n - k + 1, 0));
+
+    for (int i = 0; i <= m - k; i++) {
+      for (int j = 0; j <= n - k; j++) {
+        vector<int> values;
+        values.reserve(k * k);
+
+        for (int a = 0; a < k; a++) {
+          for (int b = 0; b < k; b++) {
+            values.push_back(grid[i + a][j + b]);
+          }
+        }
+
+        sort(values.begin(), values.end());
+
+        int min_diff = INT_MAX;
+        for (int a = 1; a < values.size(); a++) {
+          if (values[a] != values[a - 1]) {
+            min_diff = min(min_diff, values[a] - values[a - 1]);
+          }
+        }
+
+        ans[i][j] = (min_diff == INT_MAX) ? 0 : min_diff;
       }
-      if (root_one != root_two) {
-        union_find.parent[root_two] = root_one;
-        if (edges[i][3] == 1) essential_weight.push_back(edges[i][2]);
-        if (edges[i][3] == 0) upgrade_weight.push_back(edges[i][2]);
-      }
     }
-    if (essential_weight.size() + upgrade_weight.size() != n - 1) {
-      return -1;
-    }
-    sort(upgrade_weight.begin(), upgrade_weight.end());
-    for (int i = 0; i < min((int)upgrade_weight.size(), k); i++) {
-      upgrade_weight[i] *= 2;
-    }
-    int result = INT_MAX;
-    for (int w : essential_weight) result = min(result, w);
-    for (int w : upgrade_weight) result = min(result, w);
-    return result;
+    return ans;
   }
 };
 
@@ -68,31 +49,25 @@ int main() {
 
   // Test Case 1
   cout << "Test Case 1: " << endl;
-  vector<vector<int>> input = {{0, 1, 2, 1}, {1, 2, 3, 0}};
-  int result = sol.maxStability(3, input, 1);
-  int result_expected = 2;
-  cout << "result : " << result << endl;
-  cout << "result_expected : " << result_expected << endl;
+  vector<vector<int>> input = {{1, 8}, {3, -2}};
+  vector<vector<int>> result = sol.minAbsDiff(input, 2);
+  vector<vector<int>> result_expected = {{2}};
   assert(result == result_expected);
   cout << "PASSED" << endl;
 
   // Test Case 2
   cout << "Test Case 2: " << endl;
-  input = {{0, 1, 4, 0}, {1, 2, 3, 0}, {0, 2, 1, 0}};
-  result = sol.maxStability(3, input, 2);
-  result_expected = 6;
-  cout << "result : " << result << endl;
-  cout << "result_expected : " << result_expected << endl;
+  input = {{3, -1}};
+  result = sol.minAbsDiff(input, 1);
+  result_expected = {{0, 0}};
   assert(result == result_expected);
   cout << "PASSED" << endl;
 
   // Test Case 3
   cout << "Test Case 3: " << endl;
-  input = {{0, 1, 1, 1}, {1, 2, 1, 1}, {2, 0, 1, 1}};
-  result = sol.maxStability(3, input, 0);
-  result_expected = -1;
-  cout << "result : " << result << endl;
-  cout << "result_expected : " << result_expected << endl;
+  input = {{1, -2, 3}, {2, 3, 5}};
+  result = sol.minAbsDiff(input, 2);
+  result_expected = {{1, 2}};
   assert(result == result_expected);
   cout << "PASSED" << endl;
 
--- cpp/3501-3600/3567/README.md
+++ cpp/3501-3600/3567/README.md
@@ -1,8 +1,8 @@
-# 3600. Maximize Spanning Tree Stability with Upgrades
+# 3567. Minimum Absolute Difference in Sliding Submatrix
 
-[Problem Link](https://leetcode.com/problems/maximize-spanning-tree-stability-with-upgrades/description/)
+[Problem Link](https://leetcode.com/problems/minimum-absolute-difference-in-sliding-submatrix/description/)
 
 ### Review
 
-I learned about Kruskal's algorithm and the Union-Find data structure in this problem.
-At first, it was hard to implement, but I broke it down into steps, and then I solved it.
+Since the constraints are small, I choose a brute-force approach.
+It seems easy, but I feel like I'll need to review the code a few times.
