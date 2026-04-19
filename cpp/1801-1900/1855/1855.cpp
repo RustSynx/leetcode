75ca63d4eaa74776d55926286ccc2574b53e4375
Milo <pymc20@gmail.com>
2026-04-19 18:41:19 +0900

    Add Maximum Distance Between a Pair of Values solution with tests, README, and CMake updates

---
+++ cpp/1801-1900/1855/1855.cpp
@@ -0,0 +1,64 @@
+//
+// Created by Milo on 4/19/26.
+//
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+ public:
+  int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+    int result = 0;
+    int i = 0, j = 0;
+    while (i < nums1.size() && j < nums2.size()) {
+      if (nums1[i] <= nums2[j]) {
+        result = max(result, j - i);
+      } else if (nums1[i] > nums2[j]) {
+        i++;
+      }
+      j++;
+    }
+    return result;
+  }
+};
+
+int main() {
+  Solution sol;
+
+  // Test Case 1
+  cout << "Test Case 1: " << endl;
+  vector<int> nums1 = {55, 30, 5, 4, 2};
+  vector<int> nums2 = {100, 20, 10, 10, 5};
+  int result = sol.maxDistance(nums1, nums2);
+  int result_expected = 2;
+  cout << "result : " << result << endl;
+  cout << "result_expected : " << result_expected << endl;
+  assert(result == result_expected);
+  cout << "PASSED" << endl;
+
+  // Test Case 2
+  cout << "Test Case 2: " << endl;
+  nums1 = {2, 2, 2};
+  nums2 = {10, 10, 1};
+  result = sol.maxDistance(nums1, nums2);
+  result_expected = 1;
+  cout << "result : " << result << endl;
+  cout << "result_expected : " << result_expected << endl;
+  assert(result == result_expected);
+  cout << "PASSED" << endl;
+
+  // Test Case 3
+  cout << "Test Case 3: " << endl;
+  nums1 = {30, 29, 19, 5};
+  nums2 = {25, 25, 25, 25, 25};
+  result = sol.maxDistance(nums1, nums2);
+  result_expected = 2;
+  cout << "result : " << result << endl;
+  cout << "result_expected : " << result_expected << endl;
+  assert(result == result_expected);
+  cout << "PASSED" << endl;
+
+  return 0;
+}
---
+++ cpp/1801-1900/1855/README.md
@@ -0,0 +1,7 @@
+# 1855. Maximum Distance Between a Pair of Values
+
+[Problem Link](https://leetcode.com/problems/maximum-distance-between-a-pair-of-values/description/)
+
+### Review
+
+The key to solving this problem is utilizing the array's sorted property and the distance.
