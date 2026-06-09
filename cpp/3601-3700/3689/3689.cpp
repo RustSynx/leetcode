c499a5ea7b794f022c8e4b7f859cdda294ecdec3
Milo <pymc20@gmail.com>
2026-06-09 10:52:45 +0900

    Add Maximum Total Subarray Value I solution with tests, README, and CMake updates

---
+++ cpp/3601-3700/3689/3689.cpp
@@ -0,0 +1,43 @@
+//
+// Created by Milo on 6/9/26.
+//
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+ public:
+  long long maxTotalValue(vector<int>& nums, int k) {
+    long long max_val = *max_element(nums.begin(), nums.end());
+    long long min_val = *min_element(nums.begin(), nums.end());
+    return (max_val - min_val) * k;
+  }
+};
+
+int main() {
+  Solution sol;
+
+  // Test Case 1
+  cout << "Test Case 1: " << endl;
+  vector<int> nums = {1, 3, 2};
+  long long result = sol.maxTotalValue(nums, 2);
+  long long result_expected = 4;
+  cout << "result : " << result << endl;
+  cout << "result_expected : " << result_expected << endl;
+  assert(result == result_expected);
+  cout << "PASSED" << endl;
+
+  // Test Case 2
+  cout << "Test Case 2: " << endl;
+  nums = {4, 2, 5, 1};
+  result = sol.maxTotalValue(nums, 3);
+  result_expected = 12;
+  cout << "result : " << result << endl;
+  cout << "result_expected : " << result_expected << endl;
+  assert(result == result_expected);
+  cout << "PASSED" << endl;
+
+  return 0;
+}
---
+++ cpp/3601-3700/3689/README.md
@@ -0,0 +1,7 @@
+# 3689. Maximum Total Subarray Value I
+
+[Problem Link](https://leetcode.com/problems/maximum-total-subarray-value-i/description/)
+
+### Review
+
+At first, the problem seemed tricky, but I found a constraint that made it easy to solve.
