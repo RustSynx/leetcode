9550a2cbe4ad12c18fba67e6ec6ba01eeafa2bf1
Milo <pymc20@gmail.com>
2026-08-02 11:51:17 +0100

    Add Stone Game solution with tests, README, and CMake updates

---
+++ cpp/0801-0900/0877/0877.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Milo on 8/2/26.
+//
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+  bool stoneGame(vector<int>& piles) {
+    return true;
+  }
+};
+
+int main() {
+  Solution sol;
+
+  // Test Case 1
+  cout << "Test Case 1: " << endl;
+  vector<int> piles = {5, 3, 4, 5};
+  bool result = sol.stoneGame(piles);
+  bool result_expected = true;
+  assert(result == result_expected);
+  cout << "PASSED" << endl;
+
+  // Test Case 2
+  cout << "Test Case 2: " << endl;
+  piles = {3, 7, 2, 3};
+  result = sol.stoneGame(piles);
+  result_expected = true;
+  assert(result == result_expected);
+  cout << "PASSED" << endl;
+
+  return 0;
+}
---
+++ cpp/0801-0900/0877/README.md
@@ -0,0 +1,8 @@
+# 877. Stone Game
+
+[Problem Link](https://leetcode.com/problems/stone-game/description/)
+
+### Review
+
+Even though this can be solved the same way as yesterday's problem, it has different constraints.
+The length of the array and the sum of the piles are key to solving it.
